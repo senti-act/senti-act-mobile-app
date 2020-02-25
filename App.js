669ff97a6065a58ae3d616a1e0c791d346889e4c
Karol Piernikarz <piernik@MacBook-Pro-Karol.local>
2020-02-25 18:55:51 +0100

    profile screen, init react vector icons

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/Screens/HomeScreen';
 import SpendingsScreen from './src/Screens/SpendingsScreen';
 import TipsScreen from './src/Screens/TipsScreen';
 import ProfileScreen from './src/Screens/ProfileScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Competition') {
+            iconName = focused ? 'trophy' : 'trophy';
+            size = focused ? 32 : 24;
+          } else if (route.name === 'Spendings') {
+            iconName = focused ? 'tint' : 'tint';
+            size = focused ? 32 : 24;
+          } else if (route.name === 'Tips') {
+            iconName = focused ? 'info' : 'info';
+            size = focused ? 32 : 24;
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'user' : 'user';
+            size = focused ? 32 : 24;
+          }
+
+          return <FontAwesome name={iconName} size={size} color={color} />;
+         // return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'white',
+        inactiveTintColor: 'white',
+        activeBackgroundColor: '#174A5A',
+        inactiveBackgroundColor: '#174A5A',
+        style:{height:104}
+      }}
+      >
+        <Tab.Screen name="Competition" component={HomeScreen} />
         <Tab.Screen name="Spendings" component={SpendingsScreen} />
         <Tab.Screen name="Tips" component={TipsScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,21 +1,98 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  TouchableOpacity
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
 
 class ProfileScreen extends React.Component {
+  componentDidMount() {}
 
-    componentDidMount() {
+  render() {
+    return (
+      <SafeAreaView style={{width: '100%', height: '100%'}}>
+        <ScrollView
+          style={{flex: 1}}
+          contentContainerStyle={{alignItems: 'center', width: '100%', height: '100%', flexDirection: 'column'}}>
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 24}}>Profile</Text>
+          </View>
+          <View style={{flex: 3, padding:20}}>
+            <LinearGradient
+              colors={['#a6d8d5', '#71c6c0', '#38b0a4']}
+              style={{width: '100%', height: '100%', borderRadius: 10, flexDirection: 'row'}}>
+              <View style={{flex: 1, height: '100%', justifyContent: 'center'}}>
+                <Text style={{fontSize: 24, alignSelf: 'center'}}>
+                  Good morning, Christian! 
+                  </Text>
+              </View>
+              <View style={{flex: 1, height: '100%'}}>
+                <Image
+                  style={{resizeMode: 'cover', width: '100%', height: '102%'}}
+                  source={require('../Assets/easy.png')}></Image>
+              </View>
+            </LinearGradient>
+          </View>
+          <View style={{backgroundColor: 'white', width: '90%', borderRadius: 10, flexDirection: 'column' }}>
+            <TouchableOpacity style={styles.touchable}>
+                <Text style={{width:'80%', marginLeft:20, alignSelf:'center'}}>Account setting</Text>
+                <Ionicons style={{alignSelf:'center'}} size={30} name={'ios-arrow-forward'} color="black" />
+              </TouchableOpacity>
 
-    }
+              <TouchableOpacity style={styles.touchable}>
+                <Text style={{width:'80%', marginLeft:20, alignSelf:'center'}}>Notifications</Text>
+                <Ionicons style={{alignSelf:'center'}} size={30} name={'ios-arrow-forward'} color="black" />
+              </TouchableOpacity>
 
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>hmmm</Text>
-            </View>
-        );
-    }
+              <TouchableOpacity
+                style={styles.touchable}>
+                <Text style={{width:'80%', marginLeft:20, alignSelf:'center'}}>Privacy Policy</Text>
+                <Ionicons style={{alignSelf:'center'}} size={30} name={'ios-arrow-forward'} color="black" />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.touchable}>
+                <Text style={{width:'80%', marginLeft:20, alignSelf:'center'}}>Support</Text>
+                <Ionicons style={{alignSelf:'center'}} size={30} name={'ios-arrow-forward'} color="black" />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.touchable}>
+                <Text style={{width:'80%', marginLeft:20, alignSelf:'center'}}>Abour Senti.act</Text>
+                <Ionicons style={{alignSelf:'center'}} size={30} name={'ios-arrow-forward'} color="black" />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.touchable}>
+                <Text style={{width:'80%', marginLeft:20, alignSelf:'center'}}>Create user in the household</Text>
+                <Ionicons style={{alignSelf:'center'}} size={30} name={'ios-arrow-forward'} color="black" />
+              </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={[styles.touchable, {backgroundColor: 'white', borderRadius: 10, marginTop:20, marginBottom:20, width:'90%'}]}>
+                <Text style={{width:'80%', paddingLeft:20, alignSelf:'center'}}>Log out</Text>
+                <AntDesign style={{alignSelf:'center', width:'20%', paddingLeft:20}} size={20} name={'poweroff'} color="black" />
+              </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
+const styles = {
+  touchable:{
+    height:70,
+    flexDirection:'row',
+    borderBottomWidth:1,
+    borderBottomColor:'lightgray'
+  },
 
+}
 
 export default ProfileScreen;
+
