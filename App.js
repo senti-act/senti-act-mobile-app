858311e7fa59c9ffbe0e0fcbe4b94170b75e7842
Áron Kárpáti <aronkarpati@Arons-MacBook-Pro.local>
2020-03-01 18:11:51 +0100

    tipscreen scrollview is questionable

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/Screens/HomeScreen';
 import SpendingsScreen from './src/Screens/SpendingsScreen';
 import TipsScreen from './src/Screens/TipsScreen';
@@ -10,44 +11,70 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+
+function tipsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName='Tips og råd'
+      screenOptions={{
+        gestureEnabled: true,
+        headerStyle: {
+          backgroundColor: 'trasparent',
+          height:120
+          
+        },
+        headerTitleStyle: {
+          fontSize:24
+        },
+        headerTintColor: '#174a5a',
+        headerBackTitleVisible: false
+      }}
+      headerMode='float'>
+      <Stack.Screen name="Tips og råd" component={TipsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          if (route.name === 'Competition') {
-            iconName = focused ? 'trophy' : 'trophy';
-            size = focused ? 32 : 24;
-          } else if (route.name === 'Spendings') {
-            iconName = focused ? 'tint' : 'tint';
-            size = focused ? 32 : 24;
-          } else if (route.name === 'Tips') {
-            iconName = focused ? 'info' : 'info';
-            size = focused ? 32 : 24;
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'user' : 'user';
-            size = focused ? 32 : 24;
-          }
+            if (route.name === 'Competition') {
+              iconName = focused ? 'trophy' : 'trophy';
+              size = focused ? 32 : 24;
+            } else if (route.name === 'Spendings') {
+              iconName = focused ? 'tint' : 'tint';
+              size = focused ? 32 : 24;
+            } else if (route.name === 'Tips') {
+              iconName = focused ? 'info' : 'info';
+              size = focused ? 32 : 24;
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'user' : 'user';
+              size = focused ? 32 : 24;
+            }
 
-          return <FontAwesome name={iconName} size={size} color={color} />;
-         // return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'white',
-        inactiveTintColor: 'white',
-        activeBackgroundColor: '#174A5A',
-        inactiveBackgroundColor: '#174A5A',
-        style:{height:104}
-      }}
+            return <FontAwesome name={iconName} size={size} color={color} />;
+            // return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: 'white',
+          activeBackgroundColor: '#174A5A',
+          inactiveBackgroundColor: '#174A5A',
+          style: { height: 104 }
+        }}
       >
         <Tab.Screen name="Competition" component={HomeScreen} />
         <Tab.Screen name="Spendings" component={SpendingsScreen} />
-        <Tab.Screen name="Tips" component={TipsScreen} />
+        <Tab.Screen name="Tips" component={tipsStack} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
--- src/Screens/TipsScreen.js
+++ src/Screens/TipsScreen.js
@@ -1,23 +1,86 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, Dimensions, StyleSheet, FlatList, Image, SafeAreaView } from 'react-native';
+const { height, width } = Dimensions.get('window')
+
+
+import badning from '../Assets/tips/badning.png'
+import opvask from '../Assets/tips/opvask.png'
+import splidevand from '../Assets/tips/splidevand.png'
+import toilet from '../Assets/tips/toilet.png'
+import tojvask from '../Assets/tips/tojvask.png'
+import vandhanen from '../Assets/tips/vandhanen.png'
+import jebani from '../Assets/tips/jebani.png'
 
 class TipsScreen extends React.Component {
 
-    componentDidMount() {
-
+    constructor() {
+        super();
+        this.state = {
+            dataSource: [
+                { id: 1, img: tojvask, title: 'Tøjvask' },
+                { id: 2, img: badning, title: 'Badning' },
+                { id: 3, img: toilet, title: 'Toiletskylning' },
+                { id: 4, img: opvask, title: 'Opvasken' },
+                { id: 5, img: vandhanen, title: 'Brug af vandhanen' },
+                { id: 6, img: splidevand, title: 'Brug dit spildevand' }],
+        };
     }
+    componentDidMount() { }
 
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>hmmm</Text>
-            </View>
+            <>
+                <View style={styles.MainContainer}>
+                    <ScrollView >
+                        <View style={{ flex: 1, margin: 10, marginTop: 40 }}>
+                            <View style={{ flex: 1, flexDirection: 'row', width: "100%", height: 150, backgroundColor: '#BBD7E9', alignSelf: 'center', borderRadius: 15 }}>
+                                <View style={{ flex: 1, borderRadius: 15, alignItems: 'center', justifyContent: 'center' }}>
+                                    <Text style={{ fontSize: 18, color: '#174a5a' }}>Sådan kan du spare vand i hjemmet</Text>
+                                </View>
+                                <View style={{ flex: 1, borderRadius: 15, alignItems: 'center', justifyContent: 'flex-end' }}>
+                                    <Image style={{ width: width / 2, height: width / 2 }} source={jebani} />
+                                </View>
+                            </View>
+                        </View>
+                        <View style={{ flex: 3 }}>
+                            <FlatList
+                                data={this.state.dataSource}
+                                paddingHorizontal={10}
+                                scrollEnabled={false}
+                                renderItem={({ item }) => (
+                                    <View style={{ flex: 1, height: width / 2, flexDirection: 'column', padding: 5, }}>
+                                        <View style={styles.imageThumbnail}>
+                                            <Image style={{ width: width / 3, height: width / 3, }} resizeMode="contain" source={item.img} />
+                                            <Text style={{ color: '#174a5a' }}>{item.title}</Text>
+                                        </View>
+                                    </View>
+                                )}
+                                //Setting the number of column
+                                numColumns={2}
+                                keyExtractor={(item, index) => index}
+                            />
+                        </View>
+                    </ScrollView>
+                </View>
+            </>
         );
     }
-
-
 }
 
+const styles = StyleSheet.create({
+    MainContainer: {
+        justifyContent: 'center',
+        flex: 1,
+    },
+
+    imageThumbnail: {
+        backgroundColor: 'white',
+        height: "100%",
+        borderRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
 
 export default TipsScreen;
