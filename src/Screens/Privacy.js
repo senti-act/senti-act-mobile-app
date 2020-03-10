61b4ea2fc5fadc66c31a3aef7fa889987f242b4c
Gaston-N <gastonavarro98@gmail.com>
2020-03-10 14:49:54 +0100

    privacy screen

---
+++ src/Screens/Privacy.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import {Text, View, ScrollView, SafeAreaView} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {AccordionList} from 'accordion-collapse-react-native';
+
+class Privacy extends React.Component {
+  componentDidMount() {}
+
+  _body(item) {
+    return (
+      <View>
+        <Text style={styles.text}>{item.body}</Text>
+      </View>
+    );
+  }
+  _head(item) {
+    return (
+      <View style={styles.box}>
+        <Text style={styles.header}>{item.title}</Text>
+        <Ionicons size={24} name={'ios-arrow-forward'} color="#174A5A" />
+      </View>
+    );
+  }
+  render() {
+    this.state = {
+      list: [
+        {
+          title: 'Our data policy',
+          body:
+            'Data policies are norms regulating management and publication of research data. They range from recommendations to enforcements. There is much variation in their scope and content across countries and across disciplines in single countries. ',
+        },
+        {
+          title: 'Which types of data do we collect?',
+          body: "All data types are equally accepted, we don't discriminate",
+        },
+        {
+          title: 'How do we use this information?',
+          body: "For all kinds of weird stuff, you woulnd't want to know",
+        },
+        {
+          title: 'How is the information shared?',
+          body: 'internet',
+        },
+        {
+          title: 'How do I delete my data',
+          body: "can't",
+        },
+      ],
+    };
+    return (
+      <SafeAreaView style={{height: '100%', width: '100%'}}>
+        <ScrollView
+          style={{flex: 1}}
+          contentContainerStyle={{
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              width: '90%',
+              borderRadius: 10,
+              flexDirection: 'column',
+            }}>
+            <AccordionList
+              list={this.state.list}
+              header={this._head}
+              body={this._body}
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = {
+  box: {
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingRight: 30,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'normal',
+    color: '#174A5A',
+    textAlign: 'justify',
+    paddingRight: 30,
+    paddingLeft: 30,
+    lineHeight: 24,
+    paddingTop: 10,
+  },
+  header: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    color: '#174A5A',
+    paddingRight: 30,
+    paddingLeft: 30,
+  },
+};
+export default Privacy;
