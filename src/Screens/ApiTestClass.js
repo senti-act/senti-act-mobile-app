5a56d33bbaf54c696ebb9dc53283a595383e0486
Doly <DavidDolyDolyak@gmail.com>
2020-04-06 14:09:24 +0200

    ip fix

--- src/Screens/ApiTestClass.js
+++ src/Screens/ApiTestClass.js
@@ -19,38 +19,38 @@ class ApiTestClass extends React.Component {
             isLoading: true
         };
     }
-    http://192.168.87.127:4000/api/users
-        componentDidMount() {
-            fetch('https://reactnative.dev/movies.json')
-                .then((response) => response.json())
-                .then((json) => {
-                    this.setState({ data: json.movies });
-                })
-                .catch((error) => console.error(error))
-                .finally(() => {
-                    this.setState({ isLoading: false });
-                });
-        }
+    // http://localhost:4000/api/users
+    componentDidMount() {
+        fetch('https://reactnative.dev/movies.json')
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState({ data: json.movies });
+            })
+            .catch((error) => console.error(error))
+            .finally(() => {
+                this.setState({ isLoading: false });
+            });
+    }
 
 
-render() {
-    const { data, isLoading } = this.state;
-    return (
-        <SafeAreaView style={{ height: '100%', width: '100%' }}>
-            <View style={{ flex: 1, padding: 24 }}>
-                {isLoading ? <ActivityIndicator /> : (
-                    <FlatList
-                        data={data}
-                        keyExtractor={({ id }, index) => id}
-                        renderItem={({ item }) => (
-                            <Text>{item.title}, {item.releaseYear}</Text>
-                        )}
-                    />
-                )}
-            </View>
-        </SafeAreaView>
-    );
-}
+    render() {
+        const { data, isLoading } = this.state;
+        return (
+            <SafeAreaView style={{ height: '100%', width: '100%' }}>
+                <View style={{ flex: 1, padding: 24 }}>
+                    {isLoading ? <ActivityIndicator /> : (
+                        <FlatList
+                            data={data}
+                            keyExtractor={({ id }, index) => id}
+                            renderItem={({ item }) => (
+                                <Text>{item.title}, {item.releaseYear}</Text>
+                            )}
+                        />
+                    )}
+                </View>
+            </SafeAreaView>
+        );
+    }
 }
 
 
